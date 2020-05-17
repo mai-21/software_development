70161a017079c4202b312039bce374da2e962aad
mai <mai@192.168.11.7>
2020-05-17 21:51:01 +0900

    like hood.

---
+++ soft_development/src/soft_development/Like.java
@@ -0,0 +1,10 @@
+package soft_development;
+
+public class Like {
+		public static void main(String[] args) {
+			
+			System.out.println("好きな食べ物：きんぴらごぼう");
+			System.out.println("理由：最近たくさん作ってずっと食べていたら好きになりました。");
+	}
+}
+
---
+++ software_development/src/software_development/Food.java
@@ -0,0 +1,11 @@
+package software_development;
+
+public class Food {
+	public static void main(String[] args) {
+		
+		System.out.println("好きな食べ物　：　きんぴらごぼう");
+		System.out.println("理由　：　ここ最近たくさん作ってずっと食べていたら好きになりました。");
+		
+	}
+
+}
